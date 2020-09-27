5d10f7b5158497cdaf6f050f564e9d0582e1f45e
Florentino Carrera <70342537+TinoCarrera@users.noreply.github.com>
2020-09-27 13:56:52 -0400

    Configuración en AppServiceProvider:
    -Creación de función para mostrar "success" y "error".

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Session;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Inertia::share([
+            'flash' => function () {
+                return [
+                    'success' => Session::get('success'),
+                    'error' => Session::get('error'),
+                ];
+            },
+        ]);
     }
 
     /**
