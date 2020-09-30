edebfbc4c22a2e08142f2d28df49b0798be047ea
Florentino Carrera <70342537+TinoCarrera@users.noreply.github.com>
2020-09-30 01:26:22 -0400

    Mdificación de Category/Edit:
    -Modificación visual.
    -Cambio de Edit a EditCategory.
    -Modificación de ruta en el Controlador.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -74,7 +74,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        return Inertia::render('Categories/Edit', [
+        return Inertia::render('Categories/EditCategory', [
             'category' => [
                 'id' => $category->id,
                 'name' => $category->name,
--- resources/js/Pages/Categories/EditCategory.vue
+++ resources/js/Pages/Categories/EditCategory.vue
@@ -3,20 +3,48 @@
 
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Editar categoría: {{ category.name }}
+                Categorías
             </h2>
         </template>
 
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <form @submit.prevent="submit">
-                <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
-                    <text-input v-model="form.name" :error="errors.name" class="pr-6 pb-8 w-full lg:w-1/2" label="Nombre" />
-                    <text-input v-model="form.description" :error="errors.description" class="pr-6 pb-8 w-full lg:w-1/2" label="Descripción" />
+        <jet-form-section @submitted="submit">
+
+            <template #title>
+                Editar categoría
+            </template>
+
+            <template #description>
+                Edita la categoría que seleccionaste.
+            </template>
+
+            <template #form>
+                <!-- Name -->
+                <div class="col-span-6 sm:col-span-4">
+                    <jet-label for="name" value="Nombre" />
+                    <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" autocomplete="name" />
+                    <jet-input-error :message="form.error('name')" class="mt-2" />
                 </div>
-                <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex items-center">
-                    <danger-button @click.native="confirmDestroy">Eliminar categoría</danger-button>
-                    <loading-button :loading="sending" class="ml-auto">Actualizar categoría</loading-button>
+
+                <!-- Description -->
+                <div class="col-span-6 sm:col-span-4">
+                    <jet-label for="description" value="Descripción" />
+                    <jet-input id="description" type="text" class="mt-1 block w-full" v-model="form.description" />
+                    <jet-input-error :message="form.error('description')" class="mt-2" />
                 </div>
+            </template>
+
+            <template #actions>
+
+                <danger-button @click.native="confirmDestroy" class="mr-auto">Eliminar categoría</danger-button>
+
+                <jet-action-message :on="form.recentlySuccessful" class="mr-3">
+                    Editada.
+                </jet-action-message>
+
+                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Editar
+                </jet-button>
+
                 <confirmation :show="confirmingDestroy" @close="confirmingDestroy = false">
                     <template #title>
                         Eliminar categoría
@@ -33,28 +61,40 @@
                         </danger-button>
                     </template>
                 </confirmation>
-            </form>
-        </div>
+
+            </template>
+
+        </jet-form-section>
 
     </app-layout>
 </template>
 
 <script>
     import AppLayout from './../../Layouts/AppLayout'
-    import TextInput from './../../Shared/TextInput'
     import DangerButton from './../../Jetstream/DangerButton'
-    import LoadingButton from './../../Shared/LoadingButton'
     import SecondaryButton from './../../Jetstream/SecondaryButton'
     import Confirmation from './../../Shared/Confirmation'
+
+    import JetFormSection from './../../Jetstream/FormSection'
+    import JetLabel from './../../Jetstream/Label'
+    import JetInput from './../../Jetstream/Input'
+    import JetInputError from './../../Jetstream/InputError'
+    import JetActionMessage from './../../Jetstream/ActionMessage'
+    import JetButton from './../../Jetstream/Button'
 
     export default {
         components: {
             AppLayout,
-            TextInput,
             DangerButton,
-            LoadingButton,
             SecondaryButton,
             Confirmation,
+
+            JetFormSection,
+            JetLabel,
+            JetInput,
+            JetInputError,
+            JetActionMessage,
+            JetButton,
         },
         props: {
             category: Object,
