4fa361b9bceb669476b8e08b7b224abc504a6b3f
Florentino Carrera <70342537+TinoCarrera@users.noreply.github.com>
2020-10-03 15:13:11 -0700

    Modificación de AppLayout:
    -Slidebar con Tailwind (aside).

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,19 @@
                     <div class="flex">
                         <!-- Logo -->
                         <div class="flex-shrink-0 flex items-center">
+                            <button class="mr-4" aria-label="Open Menu" @click="drawer">
+                                <svg
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    viewBox="0 0 24 24"
+                                    class="w-8 h-8">
+                                    <path d="M4 6h16M4 12h16M4 18h16"></path>
+                                </svg>
+                            </button>
+
                             <a href="/dashboard">
                                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                                     LuxApp
@@ -118,6 +131,122 @@
                     </div>
                 </div>
             </div>
+
+            <transition
+                enter-class="opacity-0"
+                enter-active-class="ease-out transition-medium"
+                enter-to-class="opacity-100"
+                leave-class="opacity-100"
+                leave-active-class="ease-out transition-medium"
+                leave-to-class="opacity-0">
+                <div
+                    @keydown.esc="isOpen = false"
+                    v-show="isOpen"
+                    class="z-10 fixed inset-0 transition-opacity">
+                    <div
+                        @click="isOpen = false"
+                        class="absolute inset-0 bg-black opacity-50"
+                        tabindex="0">
+                    </div>
+                </div>
+            </transition>
+
+            <aside
+                class="transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30"
+                :class="isOpen ? 'translate-x-0' : '-translate-x-full'">
+                <span
+                    @click="isOpen = false"
+                    class="flex w-full items-center p-4 border-b">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight ml-6">
+                        LuxApp
+                    </h2>
+                </span>
+                <span
+                    @click="isOpen = false"
+                    class="flex items-center p-4 hover:bg-indigo-500 hover:text-white">
+                    <span class="mr-2">
+                        <svg
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            class="w-6 h-6">
+                            <path
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                            </path>
+                        </svg>
+                    </span>
+                    <span>
+                        <inertia-link href="/dashboard">
+                            Dashboard
+                        </inertia-link>
+                    </span>
+                </span>
+                <span
+                    @click="isOpen = false"
+                    class="flex items-center p-4 hover:bg-indigo-500 hover:text-white">
+                    <span class="mr-2">
+                        <svg
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            class="w-6 h-6">
+                            <path
+                                d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span>
+                        <inertia-link href="/categories">
+                            Categorías
+                        </inertia-link>
+                    </span>
+                </span>
+                <span
+                    @click="isOpen = false"
+                    class="flex items-center p-4 hover:bg-indigo-500 hover:text-white">
+                    <span class="mr-2">
+                        <svg
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            class="w-6 h-6">
+                            <path
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span>Acerca</span>
+                </span>
+                <span
+                    @click="isOpen = false"
+                    class="flex items-center p-4 hover:bg-indigo-500 hover:text-white">
+                    <span class="mr-2">
+                        <svg
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            class="w-6 h-6">
+                            <path 
+                                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span>Contáctanos</span>
+                </span>
+            </aside>
+
         </nav>
 
         <!-- Page Heading -->
@@ -161,16 +290,13 @@
         data() {
             return {
                 showingNavigationDropdown: false,
+                isOpen: false
             }
         },
 
         methods: {
-            switchToTeam(team) {
-                this.$inertia.put('/current-team', {
-                    'team_id': team.id
-                }, {
-                    preserveState: false
-                })
+            drawer() {
+                this.isOpen = !this.isOpen;
             },
 
             logout() {
@@ -178,6 +304,24 @@
                     window.location = '/';
                 })
             },
+        },
+
+        watch: {
+            isOpen: {
+                immediate: true,
+                handler(isOpen) {
+                    if (process.client) {
+                        if (isOpen) document.body.style.setProperty("overflow", "hidden");
+                        else document.body.style.removeProperty("overflow");
+                    }
+                }
+            }
+        },
+
+        mounted() {
+            document.addEventListener("keydown", e => {
+            if (e.keyCode == 27 && this.isOpen) this.isOpen = false;
+            });
         },
 
         computed: {
